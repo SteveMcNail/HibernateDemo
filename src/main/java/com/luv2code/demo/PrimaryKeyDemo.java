642680120cc1c3979a203f3c85e102c3fd23ae2b
Stephan Naegele <stephan.naegele@pm.me>
2018-10-10 22:59:29 +0200

    adding multiple students, specifing id-generation-method

--- src/main/java/com/luv2code/demo/PrimaryKeyDemo.java
+++ src/main/java/com/luv2code/demo/PrimaryKeyDemo.java
@@ -1,15 +1,12 @@
-package com.luv2code.com.luv2code.hibernate.demo;
+package com.luv2code.demo;
 
 import com.luv2code.demo.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-
-public class CreateStudentDemo {
-
+public class PrimaryKeyDemo {
 	public static void main(String[] args) {
-
 		// create SessionFactory
 		System.out.println("Creating SessionFactory");
 		SessionFactory sessionFactory = new Configuration()
@@ -25,14 +22,18 @@ public class CreateStudentDemo {
 			// use the session to save the student
 
 			// create the student-object
-			System.out.println("Creating new student object");
-			Student tmpStudent = new Student("Naegele", "Stephan", "some@mail.com");
+			System.out.println("Creating 3 new student object");
+			Student tmpStudent1 = new Student("Appleboom", "Mary", "Mary@mail.com");
+			Student tmpStudent2 = new Student("Melonkin", "whatsdat", "whatsdat@mail.com");
+			Student tmpStudent3 = new Student("Bonia", "whoisdat", "whoisdat@mail.com");
 			// start a transaction
 			System.out.println("Starting the transaction");
 			session.beginTransaction();
 			// save the student
 			System.out.println("Saving the student object");
-			session.save(tmpStudent);
+			session.save(tmpStudent1);
+			session.save(tmpStudent2);
+			session.save(tmpStudent3);
 			// commit the transaction
 			System.out.println("Committing the session");
 			session.getTransaction().commit();
